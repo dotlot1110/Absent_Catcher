e112fcc04e72a78ddfe901c9077256dd42dccbc4
Geon <geonulim@gmail.com>
2024-12-02 14:14:16 +0900

    feat: check attendance list

--- src/client/client.py
+++ src/client/client.py
@@ -3,6 +3,8 @@ from scapy.layers.dot11 import Dot11
 import requests
 from datetime import datetime
 
+SERVER_URL = 'https://staging.api.blccu.com'
+
 def capture_wifi_packets(duration=10):
     captured_macs = set()
 
@@ -48,14 +50,14 @@ def submit_attendance(student_id, classroom_id):
         'timestamp': datetime.now().isoformat()
     }
 
-    # Server URL
-    SERVER_URL = 'http://localhost:5000/verify-attendance'
+    # URL
+    URL = SERVER_URL + "/verify-attendance"
 
     # Send request to server
     try:
         print("Sending data to server...")
         response = requests.post(
-            url=SERVER_URL,
+            url=URL,
             json=data,
             headers={'Content-Type': 'application/json'}
         )
@@ -73,11 +75,33 @@ def submit_attendance(student_id, classroom_id):
     except Exception as e:
         print(f"Error during submission: {str(e)}")
 
+def check_attendance_list():
+    URL = SERVER_URL + "/attendance"
+
+    try:
+        response = requests.get(url=URL, headers={'Content-Type': 'application/json'})
+        result = response.json()
+        print("Printing attendance list:")
+        for attendance in result:
+            print(f"Student ID: {attendance['student_id']:<15} "
+                  f"Timestamp: {attendance['timestamp']}")
+    except Exception as e:
+        print(f"Error during attendance list check: {str(e)}")
+
+
 def main():
     print("=== Attendance Verification System ===")
-    student_id = input("Enter student ID: ")
-    classroom_id = input("Enter classroom ID (e.g., classroom_1): ")
-    submit_attendance(student_id, classroom_id)
+    print("Enter the wanted process:")
+    print("1. Verify attendance")
+    print("2. Check attendance list")
+    process = input(": ")
+
+    if process == "1":
+        student_id = input("Enter student ID: ")
+        classroom_id = input("Enter classroom ID (e.g., classroom_1): ")
+        submit_attendance(student_id, classroom_id)
+    elif process == "2":
+        check_attendance_list();
 
 if __name__ == "__main__":
     main()
